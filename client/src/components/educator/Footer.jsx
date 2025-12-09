9b7f612c6f1d7710cbf83f7f32c39b7aa1c253d0
Rishabh singh <142293834+rishxx7@users.noreply.github.com>
2025-12-09 18:52:22 +0530

    Refactor Footer component structure

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
       <div className='flex items-center gap-4'>
-        <img className='hidden md:block w-20' src={ssets.logo} alt="logo" />
+        <img className='hidden md:block w-20' src={assets.logo} alt="logo" />
         <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
           Created by Rishabh.
@@ -28,4 +28,5 @@ const Footer = () => {
 
 
 export default Footer;
+
 
